f84c88e54126d76803e65e26ee0962b95952d88c
OWLsTyLe <maxshypilkin@gmail.com>
2025-06-28 22:19:10 +0300

    customizable admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.db import models
+# from . import models
 from .models import Booking
+from unfold.admin import ModelAdmin
+from unfold.contrib.forms.widgets import WysiwygWidget
 
-class BookingAdmin(admin.ModelAdmin):
+class BookingAdmin(ModelAdmin):
     list_display = ('user', 'room', 'start_date', 'end_date', 'created_at')
     list_filter = ('room', 'start_date', 'end_date')
     search_fields = ('user__username', 'room__hotel__name')
+
 
 admin.site.register(Booking, BookingAdmin)

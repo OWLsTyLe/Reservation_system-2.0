fdab38992201b929d30319356f6712944d753618
OWLsTyLe <maxshypilkin@gmail.com>
2025-06-30 22:05:19 +0300

    update

--- first/urls.py
+++ first/urls.py
@@ -29,8 +29,9 @@ urlpatterns = [
     path('booking/', include('booking.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
 )
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

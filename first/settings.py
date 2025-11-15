e0808cdf049fc4fc733e411c4d2f9e4d7038d7cf
OWLsTyLe <maxshypilkin@gmail.com>
2025-11-15 18:29:06 +0200

    update

--- first/settings.py
+++ first/settings.py
@@ -33,28 +33,28 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",  # before django.contrib.admin
-    "unfold.contrib.filters",  # optional, if special filters are needed
-    "unfold.contrib.forms",  # optional, if special form elements are needed
-    "unfold.contrib.inlines",  # optional, if special inlines are needed
-    "unfold.contrib.import_export",  # optional, if django-import-export package is used
-    "unfold.contrib.guardian",  # optional, if django-guardian package is used
-    "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
-    # "django.contrib.admin",
-
+    "modeltranslation",  # має бути найпершим серед кастомних
+    "unfold",
+    "unfold.contrib.filters",
+    "unfold.contrib.forms",
+    "unfold.contrib.inlines",
+    "unfold.contrib.import_export",
+    "unfold.contrib.guardian",
+    "unfold.contrib.simple_history",
     "django.contrib.sites",
+
     # allauth
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
 
-    'modeltranslation',
     'main',
     'hotels',
     'restourants',
     'users',
     'booking',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

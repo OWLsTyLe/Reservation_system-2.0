e7f75758af659bda80fd884490857a5a126043b4
Andrew85-jin <andrejvizitiu857@gmail.com>
2025-11-25 00:39:36 +0200

    update

--- main/static/main/js/slider.js
+++ main/static/main/js/slider.js
@@ -24,4 +24,8 @@ controls.forEach(control => {
     });
 });
 
-showSlide(currentIndex);
+setTimeout(()=>{
+    let index = currentIndex+1;
+    if(index >=images.length)index = 0;
+    showSlide(index);
+}, 5000);
